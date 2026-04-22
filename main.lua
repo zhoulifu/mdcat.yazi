05f5dcb64e3d050d68d72f46cb7ea92244d4fcb3
lifu.zhou <lz.git@outlook.com>
2026-04-22 21:44:48 +0800

    fix: compatibility problem
    compat with yazi 26.x running in iterm2

--- main.lua
+++ main.lua
@@ -2,7 +2,8 @@ local M = {}
 
 function M:peek(job)
   local child = Command('mdcat')
-    :args({
+    :arg({
+      '--ansi',
       '--columns',
       tostring(job.area.w),
       tostring(job.file.url),
@@ -24,20 +25,20 @@ function M:peek(job)
   until count >= job.skip + job.area.h
   child:start_kill()
   if job.skip > 0 and count < job.skip + job.area.h then
-    ya.manager_emit('peek', {
+    ya.emit('peek', {
       tostring(math.max(0, count - job.area.h)),
       only_if = job.file.url,
       upper_bound = '',
     })
   else
-    ya.preview_widgets(job, { ui.Text.parse(lines):area(job.area) })
+    ya.preview_widget(job, { ui.Text.parse(lines):area(job.area) })
   end
 end
 
 function M:seek(job)
   local h = cx.active.current.hovered
   if h and h.url == job.file.url then
-    ya.manager_emit('peek', {
+    ya.emit('peek', {
       math.max(0, cx.active.preview.skip + job.units),
       only_if = job.file.url,
     })
